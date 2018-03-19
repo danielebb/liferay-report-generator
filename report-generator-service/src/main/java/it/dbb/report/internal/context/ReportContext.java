6bed4e0ac9982e5c2bc9a4cfab4450f87aa48b10
danielebb <d.busin@modalsource.com>
2018-03-19 21:57:04 +0100

    convert inputstream and file in context with XDocReport image handler objects

--- report-generator-service/src/main/java/it/dbb/report/internal/context/ReportContext.java
+++ report-generator-service/src/main/java/it/dbb/report/internal/context/ReportContext.java
@@ -1,10 +1,13 @@
 package it.dbb.report.internal.context;
 
 import java.io.Closeable;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StreamUtil;
 import com.liferay.portal.kernel.util.Validator;
 
@@ -13,9 +16,12 @@ import fr.opensagres.xdocreport.converter.Options;
 import fr.opensagres.xdocreport.core.XDocReportException;
 import fr.opensagres.xdocreport.core.document.DocumentKind;
 import fr.opensagres.xdocreport.document.IXDocReport;
+import fr.opensagres.xdocreport.document.images.ByteArrayImageProvider;
+import fr.opensagres.xdocreport.document.images.FileImageProvider;
 import fr.opensagres.xdocreport.document.registry.XDocReportRegistry;
 import fr.opensagres.xdocreport.template.IContext;
 import fr.opensagres.xdocreport.template.TemplateEngineKind;
+import fr.opensagres.xdocreport.template.formatter.FieldsMetadata;
 
 public class ReportContext implements Closeable {
 
@@ -30,7 +36,7 @@ public class ReportContext implements Closeable {
 			throws XDocReportException, IOException {
 
 		originalInputStream = inputStream;
-		
+
 		// create context java model
 		if (context != null && !context.isEmpty()) {
 
@@ -39,6 +45,8 @@ public class ReportContext implements Closeable {
 			this.context = input.createContext(context);
 			this.report = true;
 
+			injectImages(context);
+
 		} else {
 
 			this.report = false;
@@ -46,16 +54,16 @@ public class ReportContext implements Closeable {
 
 		// set the format converter
 		// search through known available conversion types from
-		for(DocumentKind documentKind : DocumentKind.values()) {
-			
-			if(documentKind.getMimeType().equals(mimeTypeIn)) {
-				
+		for (DocumentKind documentKind : DocumentKind.values()) {
+
+			if (documentKind.getMimeType().equals(mimeTypeIn)) {
+
 				options = Options.getFrom(documentKind);
 			}
 		}
-		
-		if(options == null) {
-			
+
+		if (options == null) {
+
 			options = Options.getFrom(mimeTypeIn);
 		}
 
@@ -63,18 +71,18 @@ public class ReportContext implements Closeable {
 
 			// search through known available conversion types to
 			for (ConverterTypeTo converterTypeTo : ConverterTypeTo.values()) {
-				
-				if(converterTypeTo.getMimeType().equals(mimeTypeOut)) {
-					
+
+				if (converterTypeTo.getMimeType().equals(mimeTypeOut)) {
+
 					options.to(converterTypeTo);
 				}
 			}
-			
-			if(Validator.isNull(options.getTo())) {
-				
+
+			if (Validator.isNull(options.getTo())) {
+
 				options.to(mimeTypeOut);
 			}
-			
+
 			this.convert = true;
 
 		} else {
@@ -82,9 +90,9 @@ public class ReportContext implements Closeable {
 			this.convert = false;
 		}
 	}
-	
+
 	public InputStream getOriginalInputStream() {
-		
+
 		return originalInputStream;
 	}
 
@@ -112,10 +120,10 @@ public class ReportContext implements Closeable {
 
 		return convert;
 	}
-	
+
 	@Override
 	public void close() throws IOException {
-		
+
 		StreamUtil.cleanUp(true, originalInputStream);
 	}
 
@@ -123,4 +131,42 @@ public class ReportContext implements Closeable {
 
 		return XDocReportRegistry.getRegistry().loadReport(inputStream, TemplateEngineKind.Freemarker);
 	}
+
+	protected void injectImages(Map<String, Object> context) {
+
+		context.replaceAll((key, value) -> {
+
+			if (value instanceof InputStream) {
+
+				_log.info("Replacing object " + key + " with a " + ByteArrayImageProvider.class.getName());
+
+				try {
+
+					FieldsMetadata metadata = new FieldsMetadata();
+					metadata.addFieldAsImage(key);
+					input.setFieldsMetadata(metadata);
+
+					return new ByteArrayImageProvider((InputStream) value);
+
+				} catch (IOException e) {
+
+					_log.error(e);
+				}
+
+			} else if (value instanceof File) {
+
+				_log.info("Replacing object " + key + " with a " + FileImageProvider.class.getName());
+
+				FieldsMetadata metadata = new FieldsMetadata();
+				metadata.addFieldAsImage(key);
+				input.setFieldsMetadata(metadata);
+
+				return new FileImageProvider((File) value);
+			}
+
+			return value;
+		});
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(ReportContext.class);
 }
